a7b615c684c076d9969b7f791c9dc9f895d68625
Swapnil140 <103278519+Swapnil140@users.noreply.github.com>
2022-08-18 23:59:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images
 
 var image1 = document.querySelectorAll("img")[0];
 
-image1.setAttribute("src", randomImageSource);
+image1.setAttribute("src", randomDiceImage);
 
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+var randomImageSource2 = "dice" + randomNumber2 + ".png";
 
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
